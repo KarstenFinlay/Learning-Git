05adb048ebf132a211844700315f9ad510820c36
unknown <KarstenFinlay@hotmail.com>
2019-10-14 20:58:36 +0100

    initial commit

---
+++ Learning Git/Program.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace Learning_Git
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World");
+        }
+    }
+}
